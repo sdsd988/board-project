0cb147ef7b6f41b6b845b04974782064e1cb2093
JUNGSANGYUN <sdsd98987@gmail.com>
2024-02-02 10:15:10 +0900

    #15 로그인 컨트롤러 테스트 + 시큐리티 설정을 테스트 코드에반영
    스프링 시큐리티가 자동 설정해준 '/login' 컨트롤러 테스트.
    모든 컨트롤러가 시큐리티 설정을 테스트코드에서 인식할 수 있도록 '@Import' 추가

--- fastcamput-board-project/src/test/java/com/example/demo/controller/ArticleControllerTest.java
+++ fastcamput-board-project/src/test/java/com/example/demo/controller/ArticleControllerTest.java
@@ -1,10 +1,12 @@
 package com.example.demo.controller;
 
+import com.example.demo.config.SecurityConfig;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -15,6 +17,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DisplayName("View 컨트롤러 - 게시글")
+@Import(SecurityConfig.class)
 @WebMvcTest(ArticleController.class)
 class ArticleControllerTest {
 
@@ -73,6 +76,7 @@ class ArticleControllerTest {
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
     }
 
+    @Disabled("구현 중")
     @DisplayName("[view] - (GET) 해시태그 검색 전용 페이지 - 정상 호출")
     @Test
     void givenNothing_whenRequestingArticleHashtagSearchView_thenReturnsArticleSearch() throws Exception {
