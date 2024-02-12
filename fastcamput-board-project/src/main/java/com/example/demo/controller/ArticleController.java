88fb3e8f2df0e915df4c9c9e2797ce9e77a70bcf
JUNGSANGYUN <sdsd98987@gmail.com>
2024-02-12 10:34:03 +0900

    #26 - 해시태그 검색 페이지 기능 구현
    해시태그 뷰에 모든 게시글에서 나오는 해시태그를 정리해서 보여주는 기능을 구현하기 위해,
    해시태그만 unique 하게 추출해서 보여주는 쿼리를
    querydsl로 작성
    그 외 서비스, 컨트롤러, 테스트코드 작성

--- fastcamput-board-project/src/main/java/com/example/demo/controller/ArticleController.java
+++ fastcamput-board-project/src/main/java/com/example/demo/controller/ArticleController.java
@@ -29,17 +29,16 @@ public class ArticleController {
     private final PaginationService paginationService;
 
 
-
     @GetMapping
     public String articles(@RequestParam(required = false) SearchType searchType,
                            @RequestParam(required = false) String searchValue,
-                           @PageableDefault(size = 10,sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
+                           @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
                            ModelMap map) {
 
         Page<ArticleResponse> articles = articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from);
         List<Integer> barNumbers = paginationService.getPaginationBarNumbers(pageable.getPageNumber(), articles.getTotalPages());
 
-        map.addAttribute("articles",articles);
+        map.addAttribute("articles", articles);
         map.addAttribute("paginationBarNumbers", barNumbers);
         map.addAttribute("searchTypes", SearchType.values());
         return "articles/index";
@@ -53,5 +52,23 @@ public class ArticleController {
         map.addAttribute("articleComments", article.articleCommentsResponse());
 
         return "articles/detail";
+    }
+
+    @GetMapping("/search-hashtag")
+    public String searchHashtag(@RequestParam(required = false) String searchValue,
+                                @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
+                                ModelMap map) {
+
+
+        Page<ArticleResponse> articles = articleService.searchArticlesViaHashtag(searchValue, pageable).map(ArticleResponse::from);
+        List<Integer> barNumbers = paginationService.getPaginationBarNumbers(pageable.getPageNumber(), articles.getTotalPages());
+        List<String> hashtags = articleService.getHashtags();
+
+
+        map.addAttribute("articles", articles);
+        map.addAttribute("hashtags", hashtags);
+        map.addAttribute("paginationBarNumbers", barNumbers);
+        map.addAttribute("searchType", SearchType.HASHTAG);
+        return "articles/search-hashtag";
     }
 }
--- fastcamput-board-project/src/main/java/com/example/demo/repository/ArticleRepository.java
+++ fastcamput-board-project/src/main/java/com/example/demo/repository/ArticleRepository.java
@@ -2,6 +2,7 @@ package com.example.demo.repository;
 
 import com.example.demo.domain.Article;
 import com.example.demo.domain.QArticle;
+import com.example.demo.repository.querydsl.ArticleRepositoryCustom;
 import com.querydsl.core.types.dsl.DateTimeExpression;
 import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.domain.Page;
@@ -15,6 +16,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource
 public interface ArticleRepository extends
         JpaRepository<Article, Long>,
+        ArticleRepositoryCustom,
         QuerydslPredicateExecutor<Article>,
         QuerydslBinderCustomizer<QArticle>
 
